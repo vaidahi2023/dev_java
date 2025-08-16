ff9c10145d0dd805f13baa4dfe9f737cc4322944
vaidahi2023 <vaidahi810@gmail.com>
2025-08-16 15:17:55 +0530

    comparison.

---
+++ src/Basic/Comparisonoperators.java
@@ -0,0 +1,17 @@
+package Basic;
+//comparison operators
+//--- >,<,<=,>=,==,!=
+
+public class Comparisonoperators {
+
+	public static void main(String[] args) {
+		
+		int a=10,b=20;
+		System.out.println("a>b="+(a>b));
+		System.out.println("a<b="+(a<b));
+		System.out.println("a>=b="+(a>=b));
+		System.out.println("a<=b="+(a<=b));
+		System.out.println("a==b="+(a==b));
+		System.out.println("a!=b="+(a!=b));
+	}
+}
