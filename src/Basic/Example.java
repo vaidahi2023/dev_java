1e1ece230e66ce2df1f3881227dd56eed18934c8
vaidahi2023 <vaidahi810@gmail.com>
2025-08-16 17:53:22 +0530

    scanner.

---
+++ src/Basic/Example.java
@@ -0,0 +1,22 @@
+package Basic;
+import java.util.*;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		int a;
+		float f;
+		double d;
+		System.out.println("Enter the number");
+		a=s.nextInt();
+		System.out.println("Enter the number");
+		f=s.nextFloat();
+		System.out.println("Enter the number");
+		d=s.nextDouble();
+		System.out.println("a="+a);
+		System.out.println("f="+f);
+		System.out.println("d="+d);
+	}
+}
---
+++ src/Basic/Rectangle.java
@@ -0,0 +1,12 @@
+package Basic;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		
+		int l=12,b=13,area=0;
+		area=l*b;
+		System.out.println("area="+area);
+	}
+	
+}
