c23049a14eaac749f555d61a3cf3b1e591a1c35b
vaidahi2023 <vaidahi810@gmail.com>
2025-09-23 18:41:56 +0530

    prime.

--- src/For/Prime.java
+++ src/For/Prime.java
@@ -1,5 +1,33 @@
 package For;
 
+import java.util.Scanner;
+
+//prime number 
+//2 --1,2
+//13 --1,13
 public class Prime {
 
+	public static void main(String[] args) {
+		
+		int n,i,count=0;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number");
+		n=s.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				count++;
+			}
+		}
+		if(count==2)
+		{
+			System.out.println("it is prime number");
+		}
+		else 
+		{
+			System.out.println("it is not a prime number");
+		}
+		
+	}
 }
